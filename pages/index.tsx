9a905f1df067a9ca678fdc78ed9c1b95ad31e9ff
Shlok Soni <sonishlok365@gmail.com>
2022-04-30 19:45:16 +0530

    Some Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,18 @@
-import { format, parseISO } from 'date-fns';
-import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import React from 'react';
 import Layout from '../components/Layout';
-import { getAllPosts } from '../lib/api';
-import { PostType } from '../types/post';
 
-type IndexProps = {
-  posts: PostType[];
-};
-
-export const Index = ({ posts }: IndexProps): JSX.Element => {
+export const Index = (): JSX.Element => {
   return (
     <Layout>
       <section className="pb-8">
         <h1>Hey there👋,</h1>
         <p className="text-lg">
           Shlok is a software developer, dreamer, and an adventure seeker. He
-          loves working on challenging problems. He is currently pursuing
-          Bachelor's degree in Computer Applications from LJ University,
-          Ahmedabad.
+          loves working on challenging problems.Shlok's interests include
+          programming, startups, and investing.
+          He is currently pursuing Bachelor's degree in Computer Applications
+          from LJ University, Ahmedabad.
         </p>
       </section>
 
@@ -27,11 +20,16 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
         <h1>Projects</h1>
         <hr className="pb-4"></hr>
         <Link href="https://github.com/Shlok11/rocket-api-testing">
-          <a className="text-black text-2xl font-semibold underline">Rocket🚀</a>
+          <a className="text-black text-2xl font-semibold underline">
+            Rocket🚀
+          </a>
         </Link>
 
         <p className="text-lg">
-        Rocket makes API development uncomplicated. It's a platform for building and testing your APIs. Built using Vanilla Javascript, Rocket allows users to create HTTP/s requests, as well as read their responses.
+          Rocket makes API development uncomplicated. It's a platform for
+          building and testing your APIs. Built using Vanilla Javascript, Rocket
+          allows users to create HTTP/s requests, as well as read their
+          responses.
         </p>
 
         <Link href="https://github.com/Shlok11/URL-Shortener">
@@ -41,14 +39,17 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
         </Link>
 
         <p className="text-lg">
-        Developed Shortzy using Node.js, Express, and MongoDB. Shortzy shrinks the length of the URL and makes it Short and Sweet.
+          Developed Shortzy using Node.js, Express, and MongoDB. Shortzy shrinks
+          the length of the URL and makes it Short and Sweet.
         </p>
         <Link href="https://github.com/Shlok11/meetup-react-app/tree/master">
           <a className="text-black text-2xl font-semibold underline">Meetup</a>
         </Link>
 
         <p className="text-lg">
-        Built using React.js and Firebase.Meetup is a CRUD Application where users can add New Meetups, add their favorite meetups to their favorite page.
+          Built using React.js and Firebase.Meetup is a CRUD Application where
+          users can add New Meetups, add their favorite meetups to their
+          favorite page.
         </p>
       </section>
     </Layout>
